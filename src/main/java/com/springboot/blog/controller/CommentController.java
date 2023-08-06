56a47ddfbe273683d32a034e47e19d07c4890217
shubhamkhanna <shubham20khanna@gmail.com>
2023-08-06 15:00:27 +0530

    Added code for exception handling.

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -1,11 +1,8 @@
 package com.springboot.blog.controller;
 
-
-import com.springboot.blog.entity.Comment;
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.service.CommentService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
