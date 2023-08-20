6dd42f4bf016d8e43826c5270278a79d08a0a8fb
shubhamkhanna <shubham20khanna@gmail.com>
2023-08-20 23:21:02 +0530

    Added code for created entites - role and user.

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String  name;
+}
