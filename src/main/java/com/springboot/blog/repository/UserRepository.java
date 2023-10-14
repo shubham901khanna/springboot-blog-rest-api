4a5037cb1d258895d2018b1f43e84e8fc7143cb1
shubhamkhanna <shubham20khanna@gmail.com>
2023-10-15 01:08:12 +0530

    Added code.

--- src/main/java/com/springboot/blog/repository/UserRepository.java
+++ src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package com.springboot.blog.repository;
 
-public class UserRepository {
+import com.springboot.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String Email);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
