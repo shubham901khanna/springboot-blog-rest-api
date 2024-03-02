5ae288f054d5b14b79306d7495d8f13d026f812f
shubhamkhanna <shubham20khanna@gmail.com>
2024-03-02 17:31:37 +0530

    Added code for category.

--- src/main/java/com/springboot/blog/controller/CategoryController.java
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.payload.CategoryDto;
 import com.springboot.blog.service.CategoryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -13,6 +18,34 @@ public class CategoryController {
     public CategoryController(CategoryService categoryService) {
       this.categoryService = categoryService;
     }
+
+    // Build add category Rest API
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto savedCategory =  categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    // Build get category rest api
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") Long id ) {
+        CategoryDto categoryDto = categoryService.getCategory(id);
+        return ResponseEntity.ok(categoryDto);
+    }
+
+    // build get all categories rest API
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    // build update category rest api
+    @PutMapping("{id}")
+    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable("id") Long categoryId) {
+        return ResponseEntity.ok(categoryService.updateCategory(categoryDto, categoryId));
+    }
+
 
 
 }
